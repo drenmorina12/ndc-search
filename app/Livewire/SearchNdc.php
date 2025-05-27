978bd8c761dee5e29330b4c2c629220ff9aaff10
Dren Morina <drenmorina321@gmail.com>
2025-05-27 02:36:45 +0200

    feat: add livewire SearchNdc component

---
+++ app/Livewire/SearchNdc.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+use App\Models\Ndc;
+
+class SearchNdc extends Component
+{
+    
+    public function render()
+    {
+        return view('livewire.search-ndc');
+    }
+}
