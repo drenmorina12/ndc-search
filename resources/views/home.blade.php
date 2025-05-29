7dab2fc7b0ab8a177d6c6868cc4918f4fa5d582a
Dren Morina <drenmorina321@gmail.com>
2025-05-29 05:52:45 +0200

    ui: make footer into a component

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -92,11 +92,7 @@
         </main>
 
         {{-- FOOTER --}}
-        <footer
-            class="flex flex-col sm:flex-row justify-center items-center gap-8 text-sm text-gray-600 dark:text-gray-400 p-6 bg-white dark:bg-zinc-900 border-t border-gray-200 dark:border-zinc-700">
-            <a href="https://www.tenton.co" target="_blank" rel="noopener" class="hover:underline">www.tenton.co</a>
-            <a href="mailto:hello@tenton.co" class="hover:underline">hello@tenton.co</a>
-        </footer>
+        <x-footer />
 
         @fluxScripts
 
